b62a773eb28e23f2e7f8ddf05811ba7ad6509180
sharon923 <sharonsaji6@gmail.com>
2023-08-10 11:23:21 +0530

    initial commit

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import time
 
-base_url = "https://www.flipkart.com/search?q=realme%20phones&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off"
+base_url = "https://www.flipkart.com/search?q=fridge&sid=j9e%2Cabm%2Chzg&as=on&as-show=on&otracker=AS_QueryStore_OrganicAutoSuggest_1_3_na_na_na&otracker1=AS_QueryStore_OrganicAutoSuggest_1_3_na_na_na&as-pos=1&as-type=RECENT&suggestionId=fridge%7CRefrigerators&requestId=ef4b9ec0-f7fd-4725-883a-d265c7d5f5c1&as-searchtext=fri"
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36"
 }
@@ -27,25 +27,25 @@ for retry in range(max_retries):
             exit()
 
 soup = BeautifulSoup(response.content, "html.parser")
-phone_cards = soup.find_all("div", class_="_1AtVbE")
+fridge_cards = soup.find_all("div", class_="_1AtVbE")
 
-for phone_card in phone_cards:
+for fridge_card in fridge_cards:
     try:
-        phone_name = phone_card.find("div", class_="_4rR01T").text
-        phone_price = phone_card.find("div", class_="_30jeq3").text if phone_card.find("div",
+        fridge_name = fridge_card.find("div", class_="_4rR01T").text
+        fridge_price = fridge_card.find("div", class_="_30jeq3").text if fridge_card.find("div",
                                                                                        class_="_30jeq3") else "N/A"
-        phone_rating = phone_card.find("div", class_="_3LWZlK").text if phone_card.find("div",
+        fridge_rating = fridge_card.find("div", class_="_3LWZlK").text if fridge_card.find("div",
                                                                                         class_="_3LWZlK") else "N/A"
 
         phone_data.append({
-            "Name": phone_name,
-            "Price": phone_price,
-            "Rating": phone_rating
+            "Name": fridge_name,
+            "Price": fridge_price,
+            "Rating": fridge_rating
         })
     except Exception as e:
         print("Error in extracting phone data:", e)
 
 df = pd.DataFrame(phone_data)
-df.to_csv("phone2.csv", index=False)
+df.to_csv("frige.csv", index=False)
 
 print("Data successfully extracted and CSV saved.")
